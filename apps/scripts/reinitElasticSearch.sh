a688cba825c548547779523b186ea835a10a561b
Florian Wientzek <florian.wientzek@gmx.net>
2022-02-15 12:33:17 +0000

    made the reinit work properly

--- apps/scripts/reinitElasticSearch.sh
+++ apps/scripts/reinitElasticSearch.sh
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env bash
 
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
@@ -6,14 +7,14 @@ source $CURRENT_DIR/config.sh
 {
     cur_files=$(find $ARCHIVE_DIR -name "*.pdf.json")
 
+
     for entry in $cur_files
     do
-
-          UUID_LINE=$(grep "\"id\" : \"" $ARCHIVE_DIR$entry)
-          UUID=$($UUID_LINE:31:36)
-          ## send the record to elastic search
-          curl -H "Content-Type: application/json" -XPOST "http://localhost:9200/dochauser/document/$UUID" -d @$ARCHIVE_DIR$entry
+        UUID_LINE=$(grep "\"id\" : \"" $entry | tr -d '":, ')
+        UUID=${UUID_LINE:3:35}
+        #send the record to elastic search
+        curl -H "Content-Type: application/json" -XPOST "http://localhost:9200/dochauser/document/$UUID" -d @$entry
 
     done
 
-} >> $LOG_FILE;
+} 
