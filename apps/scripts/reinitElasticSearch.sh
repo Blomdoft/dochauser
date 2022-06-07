49e1e70b7382ef7ee7bf31c37a7953cb7d2016e0
Florian Wientzek <florian.wientzek@gmx.net>
2022-06-07 15:39:54 +0200

    Solved problems in reinit script, wrong id start point, file reference mistake

--- apps/scripts/reinitElasticSearch.sh
+++ apps/scripts/reinitElasticSearch.sh
@@ -8,14 +8,14 @@ source $CURRENT_DIR/config.sh
     # delete the existing index
     curl -H "Content-Type: application/json" -XDELETE "http://localhost:9200/dochauser"
     # reinit index from file
-    curl -H "Content-Type: application/json" -XPUT "http://localhost:9200/dochauser" -d $CURRENT_DIR/elasticSearchIndex.json
+    curl -H "Content-Type: application/json" -XPUT "http://localhost:9200/dochauser" -d "@$CURRENT_DIR/elasticSearchIndex.json"
 
     # Fill the index
     cur_files=$(find $ARCHIVE_DIR -name "*.pdf.json")
     for entry in $cur_files
     do
         UUID_LINE=$(grep "\"id\" : \"" $entry | tr -d '":, ')
-        UUID=${UUID_LINE:3:35}
+        UUID=${UUID_LINE:2:36}
         #send the record to elastic search
         curl -H "Content-Type: application/json" -XPOST "http://localhost:9200/dochauser/_doc/$UUID" -d @$entry
 
