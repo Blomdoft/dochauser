f18c6d5f0ceb461094a81f7a3561fcd706cbd532
Florian Wientzek <florian.wientzek@gmx.net>
2024-01-29 13:59:28 +0100

    copy to structure now working

--- apps/scripts/copyToStructureAnalyzed.sh
+++ apps/scripts/copyToStructureAnalyzed.sh
@@ -4,14 +4,13 @@ CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 source $CURRENT_DIR/config.sh
 
 # Elasticsearch server details
-ES_HOST="192.168.2.8"
 INDEX_NAME="dochauser"
 ES_URL="http://$ES_HOST:9200/$INDEX_NAME/_search"
 
 mkdir -p "$CATEGORIZED_DIR"
 
 # Get the date 30 days ago in the format required by Elasticsearch
-DATE_30_DAYS_AGO=$(date -u +"%Y%m%dT%H%M%S.000Z" 2>/dev/null || date -u -d '30 days ago' +"%Y%m%dT%H%M%S.000Z" 2>/dev/null)
+DATE_30_DAYS_AGO=$(date -u -v-30d +"%Y%m%dT%H%M%S.000Z" 2>/dev/null || date -u -d '30 days ago' +"%Y%m%dT%H%M%S.000Z" 2>/dev/null)
 
 # Elasticsearch query to get documents with analysis
 QUERY='{
@@ -27,7 +26,7 @@ QUERY='{
       },
       "filter": {
         "exists": {
-          "field": "analysis"
+          "field": "analysis.filename"
         }
       }
     }
@@ -37,22 +36,37 @@ QUERY='{
 # Fetch documents from Elasticsearch
 RESPONSE=$(curl -s -X GET "$ES_URL" -H 'Content-Type: application/json' -d "$QUERY")
 
+echo $RESPONSE
+
 # Process each document
 echo "$RESPONSE" | jq -c '.hits.hits[]' | while read -r line; do
   DIRECTORY=$(echo "$line" | jq -r '._source.directory')
   FILENAME=$(echo "$line" | jq -r '._source.name')
+  FILENAMEDEST=$(echo "$line" | jq -r '._source.analysis.filename')
   CATEGORY_LEVEL1=$(echo "$line" | jq -r '._source.analysis.category_level1')
   CATEGORY_LEVEL2=$(echo "$line" | jq -r '._source.analysis.category_level2')
   DOCUMENT_DATE=$(echo "$line" | jq -r '._source.timestamp' | cut -c 3-8)
 
+  # Extract the part of the directory after "/archive/"
+
+
+  MODIFIED_DIRECTORY=$(echo "$DIRECTORY" | awk -F'/archive/' '{print $2}')
+  echo $MODIFIED_DIRECTORY
+
+  # Prepend your base directory
+  SOURCE_PATH="${ARCHIVE_DIR}${MODIFIED_DIRECTORY}${FILENAME}"
+
   # Generate the destination path
-  DEST_PATH="${CATEGORIZED_DIR}/${CATEGORY_LEVEL1}/${CATEGORY_LEVEL2}/${DOCUMENT_DATE}_${FILENAME}"
+  DEST_PATH="${CATEGORIZED_DIR}/${CATEGORY_LEVEL1}/${CATEGORY_LEVEL2}/${DOCUMENT_DATE}_${FILENAMEDEST}"
+
+    echo $SOURCE_PATH
+    echo $DEST_PATH
 
   # Create the destination directory tree if it doesn't exist
 
   if [ ! -f "$DEST_PATH" ]; then
     mkdir -p "$(dirname "$DEST_PATH")"
-    cp "${DIRECTORY}${FILENAME}" "${DEST_PATH}"
+    cp "${SOURCE_PATH}" "${DEST_PATH}"
   fi
 
 done
