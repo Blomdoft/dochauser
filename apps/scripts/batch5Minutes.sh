c885577c1cfdf425dcafd14ecdb47cdc130843ec
Florian Wientzek <florian.wientzek@gmx.net>
2024-01-30 16:12:48 +0100

    wait a bit for elastic search

--- apps/scripts/batch5Minutes.sh
+++ apps/scripts/batch5Minutes.sh
@@ -4,6 +4,6 @@ CURRENT_DIR=$(dirname "$(readlink -f "$0")")
 source $CURRENT_DIR/../config/config.sh
 
 $CURRENT_DIR/analyzeWithGpt.sh
-sleep(10) # Wait a bit so elastic search serves the new data.
+sleep 10 # Wait a bit so elastic search serves the new data.
 $CURRENT_DIR/copyToStructureAnalyzed.sh
 $CURRENT_DIR/syncCloud.sh
